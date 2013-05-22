074a108fafca99f6843693b91b58f05d57cfa0b5
ubourdon <bourdon.ugo@gmail.com>
2013-05-22 11:43:38 +0200

    add: mapping json to case class Tweet [SimpleWay][BlockingWay]

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,20 +1,24 @@
 package controllers
 
 import play.api._
-import libs.json.JsValue
 import libs.ws.WS
 import play.api.mvc._
 import concurrent.{Await, Future}
 import concurrent.duration.Duration
 import concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
 
 object Application extends Controller {
 
     def index = Action {
-        Ok(Await.result(retrieveMentionTweets, Duration.Inf))
+        Ok(Await.result(toJson(retrieveMentionTweets), Duration.Inf))
     }
 
-    def retrieveMentionTweets: Future[JsValue] = {
+    private def toJson(result: Future[Seq[Tweet]]): Future[JsValue] = result.map( tweet => Json.toJson(tweet) )
+
+    private def retrieveMentionTweets: Future[Seq[Tweet]] = {
         WS.url("http://search.twitter.com/search.json?q=%40ugobourdon&src=typd").withQueryString(
             "page" -> "1",
             "include_entities" -> "true",
@@ -22,13 +26,28 @@ object Application extends Controller {
         ).get()
         .map { response =>
             response.status match {
-                case 200 => response.json
-                case x => response.json
+                case 200 => response.json.asOpt[Seq[Tweet]].getOrElse(Nil)
+                case x => Nil
             }
         }
     }
 
-    // tweets mentionnant @ugobourdon : http://search.twitter.com/search.json?q=%40ugobourdon&src=typd
+    implicit val readTweet: Reads[Seq[Tweet]] =
+        (__ \ "results").read(
+            seq(
+                (__ \ "from_user").read[String] and
+                (__ \ "text").read[String]
 
-    // tweets 5 derniers favoris @ugobourdon : https://api.twitter.com/1/favorites.json?count=5&screen_name=ugobourdon
+                tupled
+            )
+        ).map( _.collect {
+            case (user, text) => Tweet(user, text)
+        })
+
+    implicit val writeTweetAsJson = Json.writes[Tweet]
 }
+
+case class Tweet(user: String, content: String/*, hashtags: List[String]*/)
+
+// tweets mentionnant @ugobourdon : http://search.twitter.com/search.json?q=%40ugobourdon&src=typd
+// tweets 5 derniers favoris @ugobourdon : https://api.twitter.com/1/favorites.json?count=5&screen_name=ugobourdon
